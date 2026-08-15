d278718fad29211e22d99bb98436132e3e7302bf
Mathia-b <153137402+Mathia-b@users.noreply.github.com>
2026-08-15 14:19:03 -0400

    Update content.js

--- content.js
+++ content.js
@@ -34,7 +34,7 @@ if (
 /* ==========================================================
    INITIALIZE
    ========================================================== */
-let calculatedUpgradeAmount = null;
+
 function initializeCalculator() {
 
   createCalculatorButton();
@@ -42,7 +42,7 @@ function initializeCalculator() {
   createCalculatorPanel();
 
 }
-
+let calculatedUpgradeAmount = null;
 
 /* ==========================================================
    CREATE FLOATING CALCULATOR BUTTON
